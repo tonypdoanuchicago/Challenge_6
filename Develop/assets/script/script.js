20668d22f988372e6eea7776527aa9351fea6a5d
tonypdoanuchicago <tonypdoan@uchicago.edu>
2024-01-18 19:27:10 -0800

    Phase 3
    Phase 3

--- Develop/assets/script/script.js
+++ Develop/assets/script/script.js
@@ -10,7 +10,7 @@ function httpGet(theUrl)
 
 function getFormattedDate(date) {
     let year = date.getFullYear();
-    let month = (1 + date.getMonth()).toString().padStart(2, '0');
+    let month = (1 + date.getMonth()).toString()
     let day = date.getDate().toString().padStart(2, '0');
   
     return month + '/' + day + '/' + year;
@@ -50,6 +50,36 @@ function city_clicked(city)  {
     let icon = json2.list[0].weather[0].icon;
     let speed = json2.list[0].wind.speed;
 
+    let dt1 = json2.list[0].dt;
+    let temp1 = json2.list[0].main.temp;
+    let humidity1 = json2.list[0].main.humidity;
+    let icon1 = json2.list[0].weather[0].icon;
+    let speed1 = json2.list[0].wind.speed;
+
+    let dt2 = json2.list[8].dt;
+    let temp2 = json2.list[8].main.temp;
+    let humidity2 = json2.list[8].main.humidity;
+    let icon2 = json2.list[8].weather[0].icon;
+    let speed2 = json2.list[8].wind.speed;
+
+    let dt3 = json2.list[16].dt;
+    let temp3 = json2.list[16].main.temp;
+    let humidity3 = json2.list[16].main.humidity;
+    let icon3 = json2.list[16].weather[0].icon;
+    let speed3 = json2.list[16].wind.speed;
+
+    let dt4 = json2.list[24].dt;
+    let temp4 = json2.list[24].main.temp;
+    let humidity4 = json2.list[24].main.humidity;
+    let icon4 = json2.list[24].weather[0].icon;
+    let speed4 = json2.list[24].wind.speed;
+
+    let dt5 = json2.list[32].dt;
+    let temp5 = json2.list[32].main.temp;
+    let humidity5 = json2.list[32].main.humidity;
+    let icon5 = json2.list[32].weather[0].icon;
+    let speed5 = json2.list[32].wind.speed;
+
     document.getElementById("icon_image").src = "http://openweathermap.org/img/w/" + icon + ".png";
     document.getElementById("icon_image").style.display = "block";
 
@@ -58,4 +88,51 @@ function city_clicked(city)  {
     document.getElementById("humidity").innerHTML = "Humidity: " + humidity + " %";
 
 
+    document.getElementById("icon_image1").src = "http://openweathermap.org/img/w/" + icon1 + ".png";
+    document.getElementById("icon_image1").style.display = "block";
+
+    document.getElementById("day1").innerHTML += getFormattedDate(new Date(dt1 * 1000)) + "<br/><br/>";
+    document.getElementById("day1").innerHTML += "Temp: " + round(((temp - 273.15) * 1.8 + 32)) + " F" + "<br/><br/>";
+    document.getElementById("day1").innerHTML += "Wind: " + speed + " MPH" + "<br/><br/>";
+    document.getElementById("day1").innerHTML += "Humidity: " + humidity + " %";
+
+    ///////
+
+    document.getElementById("icon_image2").src = "http://openweathermap.org/img/w/" + icon2 + ".png";
+    document.getElementById("icon_image2").style.display = "block";
+
+    document.getElementById("day2").innerHTML += getFormattedDate(new Date(dt2 * 1000)) + "<br/><br/>";
+    document.getElementById("day2").innerHTML += "Temp: " + round(((temp2 - 273.15) * 1.8 + 32)) + " F" + "<br/><br/>";
+    document.getElementById("day2").innerHTML += "Wind: " + speed2 + " MPH" + "<br/><br/>";
+    document.getElementById("day2").innerHTML += "Humidity: " + humidity2 + " %";
+
+    ////////
+
+    document.getElementById("icon_image3").src = "http://openweathermap.org/img/w/" + icon3 + ".png";
+    document.getElementById("icon_image3").style.display = "block";
+
+    document.getElementById("day3").innerHTML += getFormattedDate(new Date(dt3 * 1000)) + "<br/><br/>";
+    document.getElementById("day3").innerHTML += "Temp: " + round(((temp3 - 273.15) * 1.8 + 32)) + " F" + "<br/><br/>";
+    document.getElementById("day3").innerHTML += "Wind: " + speed3 + " MPH" + "<br/><br/>";
+    document.getElementById("day3").innerHTML += "Humidity: " + humidity3 + " %";
+
+    //////
+
+    document.getElementById("icon_image4").src = "http://openweathermap.org/img/w/" + icon4 + ".png";
+    document.getElementById("icon_image4").style.display = "block";
+
+    document.getElementById("day4").innerHTML += getFormattedDate(new Date(dt4 * 1000)) + "<br/><br/>";
+    document.getElementById("day4").innerHTML += "Temp: " + round(((temp4 - 273.15) * 1.8 + 32)) + " F" + "<br/><br/>";
+    document.getElementById("day4").innerHTML += "Wind: " + speed4 + " MPH" + "<br/><br/>";
+    document.getElementById("day4").innerHTML += "Humidity: " + humidity4 + " %";
+
+    //////
+
+    document.getElementById("icon_image5").src = "http://openweathermap.org/img/w/" + icon5 + ".png";
+    document.getElementById("icon_image5").style.display = "block";
+
+    document.getElementById("day5").innerHTML += getFormattedDate(new Date(dt5 * 1000)) + "<br/><br/>";
+    document.getElementById("day5").innerHTML += "Temp: " + round(((temp5 - 273.15) * 1.8 + 32)) + " F" + "<br/><br/>";
+    document.getElementById("day5").innerHTML += "Wind: " + speed5 + " MPH" + "<br/><br/>";
+    document.getElementById("day5").innerHTML += "Humidity: " + humidity5 + " %";
 }
